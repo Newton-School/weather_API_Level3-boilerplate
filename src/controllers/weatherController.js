c75f5ceb8a74306ecffe5d248ce5249793b7ac5c
prakharmishra-cyber <mprakhar35@gmail.com>
2023-06-21 16:10:03 +0530

    done

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -26,6 +26,26 @@ async function saveDataToDatabase(data) {
 }
 
 
+/*
+  Instructions for students:
+  Implement the function to retrieve the weather data for a city by its zipcode.
+
+  Function:
+    getWeatherDataByZipCode(zipCode)
+
+  Input:
+    - zipCode (string): The zipcode of the city for which weather data needs to be retrieved.
+
+  Output:
+    - If the city zipcode is found in the database, return the weather data object for the specified city.
+    - If the city zipcode is not found in the database, return new Error('ZipCode not found').
+
+  Tips:
+    - Use the getDataFromDatabase() function to retrieve the data from the database.
+    - Handle the data and filter the weather data based on the provided zipcode.
+    - Return the filtered weather data or null based on the search result.
+*/
+
 
 // Level 3: Get City Weather Data by ZipCode
 async function getWeatherDataByZipCode(zipCode) {
